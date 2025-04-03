d1674f85211d333e3ed55c517758ebd310d90383
gitboyzcf <boyzcf@qq.com>
2025-04-03 18:08:48 +0800

    main: Added

--- themes/bcxm/scripts/events/welcome.js
+++
@@ -1,9 +0,0 @@
-const logger = require('hexo-log')()
-
-hexo.on('ready', () => {
-  const { version } = require('../../package.json')
-  logger.info(`
-  --------------------------bcxm--------------------------
-                            ${version}       
-`)                
-})
